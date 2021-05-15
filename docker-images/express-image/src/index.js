486dcd4598fbc1742ee20fa98ff050841c065d6a
Emmanuel Janssens <emmanueljanssens72@gmail.com>
2021-05-15 22:38:02 +0200

    Cleaning node_modules

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -16,31 +16,40 @@ app.listen(3000, function(){
 
 
 function generateStudents(){
-    var numStudents = chance.integer({
+    var numCountries = chance.integer({
         min:0,
         max:10
     });
 
-    console.log(numStudents);
-    var students= [];
+    console.log(numCountries);
+    var countries = [];
 
-    for(var i = 0; i < numStudents; i++){
-        var gender = chance.gender();
-        var birthYear = chance.year({
-            min: 1986,
-            max: 1996
+    for(var i = 0; i < numCountries; i++){
+
+        var independence = chance.date({
+            american: false,
+            string: true,
+            year: chance.integer({ min: 1600, max: 2000}),
+            month: chance.integer({min:1,max: 12}),
+            day:chance.integer({min: 1,max: 31})
         });
-        students.push({     
-            firstName: chance.first({
-                gender: gender
-            }),
-            lastName: chance.last(),
-            gender: gender,
-            birthday: chance.birthday({
-                year: birthYear
-            })
-        });
+        
+        countries.push({
+            name:chance.word(
+                chance.integer({
+                    min: 3,
+                    max: 20
+                })),
+            language:chance.word(
+                chance.integer({
+                    min: 3,
+                    max: 20
+                })),
+            independenceDay: independence,
+            population: chance.integer({min: 400,max: 30000000})
+
+        })        
     };
-    console.log(students);
-    return students;
+    console.log(countries);
+    return countries;
 }
